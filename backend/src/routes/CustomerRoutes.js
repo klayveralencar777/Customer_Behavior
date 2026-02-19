66417420e9814ed43721852d38195687ca93d24e
klayveralencar777 <alencar.klayver7@gmail.com>
2026-02-18 23:05:48 -0300

    swagger: add customers

--- backend/src/routes/CustomerRoutes.js
+++ backend/src/routes/CustomerRoutes.js
@@ -5,12 +5,11 @@ const router = express.Router();
 const customerController = new CustomerController();
 router.use(authMiddleware);
 router.get('/', customerController.findCustomers.bind(customerController));
-router.get('/:id', customerController.findCustomerById.bind(customerController));
 router.get('/email/:email', customerController.findCustomerByEmail.bind(customerController));
+router.get('/:id', customerController.findCustomerById.bind(customerController));
 router.post('', customerController.createCustomer.bind(customerController));
 router.put('/:id', customerController.updateCustomer.bind(customerController));
 router.delete('/:id', customerController.deleteCustomer.bind(customerController));
-
 
 
 
